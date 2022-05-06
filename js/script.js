9e396b275336bcb4e1e0d81b3c659e2df7be2d1e
Neda Gholami <nedagholami1998@gmail.com>
2022-05-06 15:29:52 -0700

    add new files

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const textEl = document.getElementById('text');
+const speedEl = document.getElementById('speed');
+const text = 'we love programming';
+let idx = 1;
+let speed = 300 / speedEl.value;
+
+
+
+
+function writeText() {
+    textEl.innerText = text.slice(0, idx);
+    
+    idx++;
+    if(idx > text.length){
+        idx = 1;
+    }
+setTimeout(writeText, speed)
+}
+ writeText();
+
+ speedEl.addEventListener('input', (e) => {
+     speed = 300 / e.target.value
+ })
